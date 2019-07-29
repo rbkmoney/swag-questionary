48f9cc9b270861dd0691e7b9f124c9c47bd66186
vbanin <vitaxa93gamebox@gmal.com>
2019-07-29 17:35:55 +0300

    review fix

--- gulpfile.js
+++ gulpfile.js
@@ -9,6 +9,7 @@ var portfinder = require('portfinder');
 var swaggerRepo = require('swagger-repo');
 
 var DIST_DIR = 'web_deploy';
+var SPEC_DIR = 'spec';
 
 gulp.task('serve', ['build', 'watch', 'edit'], function() {
   portfinder.getPort({port: 3000}, function (err, port) {
@@ -46,5 +47,5 @@ gulp.task('reload', ['build'], function () {
 });
 
 gulp.task('watch', function () {
-  gulp.watch(['api/swag-questionary/**/*', 'web/**/*'], ['reload']);
+  gulp.watch([`${SPEC_DIR}/**/*`, 'web/**/*'], ['reload']);
 });
